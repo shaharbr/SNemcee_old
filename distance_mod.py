e758926ab75f5a8885a90353ea46679b249c53df
Shahar Bracha <shaharbra@gmail.com>
2020-11-13 21:09:29 +0200

    calculate distance modulus from Luminosity Distance or from redshift z

--- distance_mod.py
+++ distance_mod.py
@@ -1,6 +1,32 @@
-from astropy import cosmology
-from astropy.coordinates import Distance
-cosmology.default_cosmology.set(cosmology.WMAP7)
-redshift = 0.025
-distmod = Distance(z=redshift).distmod.value
-print(distmod)
+import numpy as np
+
+
+
+def distance_luminosity_to_modulus(dL, dL_err):
+    dm = 5 * np.log10(dL) - 5
+    dm_err = 5 / (np.log(10) * dL) * dL_err
+    return dm, dm_err
+
+
+dL = 159 * 10**6
+dL_err = 15.9 * 10**6
+dm, dm_err = distance_luminosity_to_modulus(dL, dL_err)
+print(dm)
+print(dm_err)
+
+
+'''
+# dL values from:
+1) "The distance, supernova rate, and supernova progenitors of NGC 6946" J J Eldridge, Lin Xiao 2019
+
+'''
+
+
+
+
+# from astropy import cosmology
+# from astropy.coordinates import Distance
+# cosmology.default_cosmology.set(cosmology.WMAP7)
+# redshift = 0.000133
+# distmod = Distance(z=redshift).distmod.value
+# print(distmod)
