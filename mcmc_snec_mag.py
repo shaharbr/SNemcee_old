arrange code, fix log likelihood calculation, remove chisquare usage, remove redundant fitting function and coordinate all mcmc codes to be more similar, fix the value recieved if beyoond the range of parameter values allowed, fix plotting of fitting result, fix issue with the day of explosion parameter

mcmc_snec_mag.py
@@ -14,38 +14,26 @@ import csv
 parts of this code are based on code by Griffin Hosseinzadeh
 '''
 
-
-plt.rc('font', size=20)          # controls default text sizes
-plt.rc('axes', titlesize=20)     # fontsize of the axes title
-plt.rc('axes', labelsize=20)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=20)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=20)    # fontsize of the tick labels
-plt.rc('legend', fontsize=14)    # legend fontsize
-plt.rc('figure', titlesize=30)  # fontsize of the figure title
-plt.rcParams['font.sans-serif'] = 'Arial'
-
-
-# important to note: values can't be negative!
-Mzams_range = [9.0, 9.0]
-Ni_range = [0.02, 0.02]
-E_final_range = [1.2, 1.2]
-Mix_range = [1.0, 1.0]
-R_range = [600, 1400, 2200, 3000]
-K_range = [0.001, 50, 100, 150]
-S_range = [0.5, 1.5]
-T_range = [0, 15] # because can't have negative values, do 15 minus diff (so 0 is -15, and 30 is +15)
+SN_name = 'SN2017eaw'
+Mzams_range = [9.0, 11.0, 13.0, 15.0, 17.0]
+Ni_range = [0.02, 0.07, 0.12, 0.17]
+E_final_range = [0.7, 1.0, 1.3, 1.9]
+Mix_range = [2.0, 3.0]
+R_range = [600, 1500, 3000]
+K_range = [0.001, 120]
+S_range = [0.9, 1.1]
+T_range = [15, 30] # because can't have negative values, do 15 minus diff (so 0 is -15, and 30 is +15)
 pysynphot_models = True
 
-
-n_walkers = 16
-n_steps = 1
+n_walkers = 20
+n_steps = 200
 n_params = 8
-burn_in = 0
-
+burn_in = 150
 
 
 time_now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-Path(os.path.join('mcmc_results', str(time_now)+'_mag')).mkdir(parents=True, exist_ok=True)
+res_dir = os.path.join('mcmc_results', str(time_now)+'_mag')
+Path(res_dir).mkdir(parents=True, exist_ok=True)
 
 run_param_df = pd.DataFrame.from_dict({'Mzams_range': str(Mzams_range), 'Ni_range': str(Ni_range),
                              'E_final_range': str(E_final_range), 'R_range': str(R_range), 'K_range': str(K_range),
@@ -54,21 +42,18 @@ run_param_df = pd.DataFrame.from_dict({'Mzams_range': str(Mzams_range), 'Ni_rang
                              'burn_in': burn_in,
                              'time': time_now,
                              'pysynphot_model': str(pysynphot_models)}, orient='index')
-run_param_df.to_csv(os.path.join('mcmc_results', str(time_now)+'_mag', 'run_parameters.csv'))
-
+run_param_df.to_csv(os.path.join(res_dir, 'run_parameters.csv'))
 
 m_Solar = 1.989 * (10 ** 33)  # gram
 
-# import mag data
-data_filepath = os.path.join('results', 'SN2018hmx_lightcurves')
+# import SN mag data
+data_filepath = os.path.join('results', SN_name+'_lightcurves')
 SN = pd.read_csv(data_filepath, usecols=['dmag', 'filter', 'abs_mag', 't_from_discovery'])
 if pysynphot_models:
     SN = SN.loc[SN['t_from_discovery'] < 140]
 SN['abs_mag'] = SN['abs_mag'].abs()
 filters = list(SN['filter'].unique())
-# filters = list(set(filters).intersection(['u', 'g', 'r', 'i', 'z', 'U', 'B', 'V', 'R', 'I']))
-filters = list(set(filters).intersection(['g', 'r', 'i']))
-
+filters = list(set(filters).intersection(['g', 'r', 'i', 'V', 'R', 'I']))
 SN = SN.loc[SN['filter'].isin(filters)]
 SN = SN.sort_values('t_from_discovery')
 colors = {'u': 'purple', 'g': 'teal', 'r': 'red', 'i': 'maroon', 'z': 'black', 'U': 'purple',
@@ -80,6 +65,15 @@ if times_to_amplify > 1:
     last_row = SN.loc[SN['t_from_discovery'] > 350]
     last_row_repeats = pd.concat([last_row]*(times_to_amplify-1), ignore_index=True)
     SN = pd.concat([SN, last_row_repeats], ignore_index=True)
+
+plt.rc('font', size=20)          # controls default text sizes
+plt.rc('axes', titlesize=20)     # fontsize of the axes title
+plt.rc('axes', labelsize=20)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=20)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=20)    # fontsize of the tick labels
+plt.rc('legend', fontsize=14)    # legend fontsize
+plt.rc('figure', titlesize=30)  # fontsize of the figure title
+plt.rcParams['font.sans-serif'] = 'Arial'
 
 
 def log_prior(theta):
@@ -150,28 +144,26 @@ def log_likelihood(theta, data):
             (T_range[0] <= theta[7] <= T_range[-1]):
         log_likeli = 0
         data_x_allfilt = data['t_from_discovery'] - 15 + theta[7]
+        filters = list(data['filter'].unique())
         y_fit = interp.snec_interpolator(theta[0:6], sampled, data_x_allfilt, filters, pysynphot_models)
         # multiply whole graph by scaling factor
         y_fit = y_fit * theta[6]
         y_fit['time'] = data_x_allfilt
-        # TODO yfilt needs to be a df with all filters and a column for time
-        # data_filt = data.keys() #  TODO use this but ommit time column
-        # TODO filters here is taken from outside function - should get it in inputs to the function (which should get it from upstream funcitons)
         for filt in filters:
             data_filt = data.loc[data['filter'] == filt]
-            data_x = data_filt['t_from_discovery'] - 15 + theta[7]
-            data_y = data_filt['abs_mag']
-            data_dy = data_filt['dmag']
-            y_fit_filt = y_fit[filt].loc[y_fit['time'].isin(data_x)]
-            if not len(data_x) == len(y_fit_filt):
+            data_x_filt = data_filt['t_from_discovery'] - 15 + theta[7]
+            data_y_filt = data_filt['abs_mag']
+            data_dy_filt = data_filt['dmag']
+            y_fit_filt = y_fit[filt].loc[y_fit['time'].isin(data_x_filt)]
+            if not len(data_x_filt) == len(y_fit_filt):
                 print('stuck')
-            log_likeli += -np.sum((data_y - y_fit_filt) ** 2. / (2. * data_dy ** 2.) + np.log(data_y))
+            log_likeli += -np.sum((data_y_filt - y_fit_filt) ** 2. / (2. * data_dy_filt ** 2.)
+                                  + np.log(data_dy_filt))
             print('log likelihood', log_likeli)
     else:
-        log_likeli = 100000000000000
+        log_likeli = - 10 ** 30  # just a very big number so it won't go past the edge values
         print('not valid')
-    print('likelihood_log', log_likeli)
-    print('likelihood', np.exp(log_likeli))
+    print('log likelihood', log_likeli)
     return log_likeli
 
 
@@ -184,6 +176,7 @@ def log_posterior(theta, data):
 
 def emcee_fit_params(data):
     sampler = emcee.EnsembleSampler(n_walkers, n_params, log_posterior, args=[data])
+
     Mzams_random = np.random.rand(n_walkers) * (Mzams_range[-1] - Mzams_range[0]) + Mzams_range[0]
     Ni_random = np.random.rand(n_walkers) * (Ni_range[-1] - Ni_range[0]) + Ni_range[0]
     E_random = np.random.rand(n_walkers) * (E_final_range[-1] - E_final_range[0]) + E_final_range[0]
@@ -208,56 +201,56 @@ def chain_plots(sampler, **kwargs):
     plt.xlabel('Step Number')
     plt.ylabel('Mzams')
     plt.tight_layout()
-    f_Mzams.savefig(os.path.join('mcmc_results', str(time_now)+'_mag', 'Mzams.png'))
+    f_Mzams.savefig(os.path.join(res_dir, 'Mzams.png'))
 
     f_Ni = plt.figure()
     plt.plot(chain[:, :, 1].T, **kwargs)
     plt.xlabel('Step Number')
     plt.ylabel('Ni')
     plt.tight_layout()
-    f_Ni.savefig(os.path.join('mcmc_results', str(time_now)+'_mag', 'Ni.png'))
+    f_Ni.savefig(os.path.join(res_dir, 'Ni.png'))
 
     f_E = plt.figure()
     plt.plot(chain[:, :, 2].T, **kwargs)
     plt.xlabel('Step Number')
     plt.ylabel('E')
     plt.tight_layout()
-    f_E.savefig(os.path.join('mcmc_results', str(time_now)+'_mag', 'E.png'))
+    f_E.savefig(os.path.join(res_dir, 'E.png'))
 
     f_R = plt.figure()
     plt.plot(chain[:, :, 3].T, **kwargs)
     plt.xlabel('Step Number')
     plt.ylabel('R')
     plt.tight_layout()
-    f_R.savefig(os.path.join('mcmc_results', str(time_now)+'_mag', 'R.png'))
+    f_R.savefig(os.path.join(res_dir, 'R.png'))
 
     f_K = plt.figure()
     plt.plot(chain[:, :, 4].T, **kwargs)
     plt.xlabel('Step Number')
     plt.ylabel('K')
     plt.tight_layout()
-    f_K.savefig(os.path.join('mcmc_results', str(time_now)+'_mag', 'K.png'))
+    f_K.savefig(os.path.join(res_dir, 'K.png'))
 
     f_Mix = plt.figure()
     plt.plot(chain[:, :, 5].T, **kwargs)
     plt.xlabel('Step Number')
     plt.ylabel('Mixing')
     plt.tight_layout()
-    f_Mix.savefig(os.path.join('mcmc_results', str(time_now)+'_mag', 'Mix.png'))
+    f_Mix.savefig(os.path.join(res_dir, 'Mix.png'))
 
     f_S = plt.figure()
     plt.plot(chain[:, :, 6].T, **kwargs)
     plt.xlabel('Step Number')
     plt.ylabel('Scaling')
     plt.tight_layout()
-    f_S.savefig(os.path.join('mcmc_results', str(time_now)+'_mag', 'S.png'))
+    f_S.savefig(os.path.join(res_dir, 'S.png'))
 
     f_T = plt.figure()
     plt.plot(chain[:, :, 7].T, **kwargs)
     plt.xlabel('Step Number')
     plt.ylabel('T_exp')
     plt.tight_layout()
-    f_T.savefig(os.path.join('mcmc_results', str(time_now)+'_mag', 'T.png'))
+    f_T.savefig(os.path.join(res_dir, 'T.png'))
 
 
 
@@ -274,7 +267,7 @@ def get_param_results_dict(sampler, step):
         dict[params[i] + '_upper'] = sigma_upper - avg
     print(dict)
 
-    with open(os.path.join('mcmc_results', str(time_now)+'_mag', 'final_results.csv'), 'w') as f:  # Just use 'w' mode in 3.x
+    with open(os.path.join(res_dir, 'final_results.csv'), 'w') as f:  # Just use 'w' mode in 3.x
         w = csv.DictWriter(f, dict.keys())
         w.writeheader()
         w.writerow(dict)
@@ -286,7 +279,7 @@ def plot_with_markers(SN_filt_data, y_fit, filter):
     plt.plot(SN_filt_data['t_from_discovery'], SN_filt_data['abs_mag'], marker='o')
     plt.plot(SN_filt_data['t_from_discovery'], y_fit, marker='o')
     plt.tight_layout()
-    f_mark.savefig(os.path.join('mcmc_results', str(time_now)+'_mag', 'markers_'+filter+'.png'))
+    f_mark.savefig(os.path.join(res_dir, 'markers_'+filter+'.png'))
 
 
 def plot_lightcurve_with_fit(SN_data, sampler, step):
@@ -306,61 +299,61 @@ def plot_lightcurve_with_fit(SN_data, sampler, step):
                    ' K: ' + str(int(K)) + \
                    ' Mix: ' + str(round(Mix, 1)) + \
                    ' S: ' + str(round(S, 2)) + \
-                   ' T: ' + str(round(T, 1))
+                   ' T: '+str(round(T - 15, 1))
     print(results_text)
 
-    requested = [Mzams, Ni, E, R, K, Mix]
+    requested = [Mzams, Ni, E, R, K, Mix, S, T]
     sampled = [Mzams_range, Ni_range, E_final_range, R_range, K_range, Mix_range]
-    # TODO here too - filters sould be got from function inputs
-    data_x_allfilt = SN_data['t_from_discovery']
+    filters = list(SN_data['filter'].unique())
     f_fit, ax = plt.subplots(figsize=(10, 8))
-    # TODO here too filters
-    log_likeli = 0
     for filt in filters:
         data_filt = SN_data.loc[SN_data['filter'] == filt]
         data_x = data_filt['t_from_discovery']
-        data_x_moved = data_x + T
+        data_x_moved = data_x + -15 + T
         data_y = data_filt['abs_mag']
         data_dy = data_filt['dmag']
-        y_fit_filt = interp.snec_interpolator(requested, sampled, data_x_moved, [filt], pysynphot_models)[filt]
+        y_fit_filt = interp.snec_interpolator(requested[0:6], sampled, data_x_moved, [filt], pysynphot_models)[filt]
         y_fit_filt = y_fit_filt * S
-        log_likeli += -np.sum((data_y - y_fit_filt) ** 2. / (2. * data_dy ** 2.) + np.log(data_y))
-        print('log likelihood', log_likeli)
         ax.plot(data_x_moved, y_fit_filt, color=colors[filt])
-        ax.errorbar(data_x_moved, data_y, yerr=data_dy, marker='o', linestyle='None', label=filt + '_SN 2018hmx', color=colors[filt])
+        ax.errorbar(data_x_moved, data_y, yerr=data_dy, marker='o', linestyle='None',
+                    label=filt+' '+SN_name, color=colors[filt])
+    log_likeli = log_likelihood(requested, SN_data)
+    print('log likelihood', log_likeli)
     ax.set_title('step '+str(step)+'\nlog likelihood = ' + str(int(log_likeli)) +
                  '\n snec model: ' + results_text, fontsize=14)
     plt.tight_layout()
     ax.legend()
-    f_fit.savefig(os.path.join('mcmc_results', str(time_now)+'_mag', 'lightcurve_fit.png'))
+    f_fit.savefig(os.path.join(res_dir, 'lightcurve_fit' +str(step) +'.png'))
+
 
 
 
 sampler = emcee_fit_params(SN)
+results_vec = plot_lightcurve_with_fit(SN, sampler, n_steps-1)
+# results_vec = plot_lightcurve_with_fit(SN, sampler, 50)
+# results_vec = plot_lightcurve_with_fit(SN, sampler, 20)
+results_vec = plot_lightcurve_with_fit(SN, sampler, 1)
+
 # to correct for T (time after explostion) actually being T+15
 sampler.chain[:, :, 7] = sampler.chain[:, :, 7] - 15
 chain_plots(sampler)
-# results_vec = plot_lightcurve_with_fit(SN, sampler, 0)
-# results_vec = plot_lightcurve_with_fit(SN, sampler, 3)
-results_vec = plot_lightcurve_with_fit(SN, sampler, n_steps-1)
 
 
 flat_sampler = sampler.get_chain(flat=True)
-np.savetxt(os.path.join('mcmc_results', str(time_now)+'_mag', 'flat_sampler.csv'), flat_sampler, delimiter=",")
+np.savetxt(os.path.join(res_dir, 'flat_sampler.csv'), flat_sampler, delimiter=",")
 
 flat_sampler_no_burnin = sampler.get_chain(discard=burn_in, flat=True)
-np.savetxt(os.path.join('mcmc_results', str(time_now)+'_mag', 'flat_sampler_excluding_burnin.csv'), flat_sampler_no_burnin, delimiter=",")
+np.savetxt(os.path.join(res_dir, 'flat_sampler_excluding_burnin.csv'), flat_sampler_no_burnin, delimiter=",")
 
 
 labels = ['Mzams', 'Ni', 'E', 'R', 'K', 'Mix', 'S', 'T']
 corner_range = [1., 1., 1., 1., 1., 1., 1., 1.]
 f_corner = corner.corner(flat_sampler_no_burnin, labels=labels, range=corner_range)
 # plt.tight_layout()
-f_corner.savefig(os.path.join('mcmc_results', str(time_now)+'_mag', 'corner_plot.png'))
+f_corner.savefig(os.path.join(res_dir, 'corner_plot.png'))
 
 # MCMC_results = get_param_results_dict(sampler)
 
 print(sampler.chain.shape)
 
-plt.show()
 