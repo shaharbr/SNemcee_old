fix plotting, add velocity scaling factor

mcmc_snec.py
@@ -29,7 +29,9 @@ plt.rcParams['font.sans-serif'] = 'Arial'
 colors = {'u': 'purple', 'g': 'teal', 'r': 'red', 'i': 'maroon', 'z': 'black', 'U': 'purple',
           'B': 'blue', 'V': 'green', 'R': 'red', 'I': 'maroon'}
 
-
+# multiplicative factor for SNEC's predicted photospheric velocities to Fe II velocities,
+# found to be equal about 1.4, by fitting to all the SNe in our sample
+Sv = 1.4
 
 def theta_in_range(theta, ranges_dict):
     ranges_list = dict_to_list(ranges_dict)
@@ -106,6 +108,8 @@ def calc_veloc_likelihood(theta, data, ranges_list):
     data_dy = data['dveloc']
     y_fit = interp_veloc.snec_interpolator(theta[0:6], ranges_list, data_x_moved)
     if not isinstance(y_fit, str):
+        # multiply whole graph by scaling factor
+        y_fit = y_fit * Sv
         # calculate the log likelihood
         log_likeli = - 0.5 * np.sum(np.log(2 * np.pi * data_dy ** 2) + ((data_y - y_fit) / data_dy) ** 2)
     else:
@@ -155,7 +159,7 @@ def log_likelihood(theta, data, ranges_dict, fitting_type):
     print(theta)
     ranges_list = dict_to_list(ranges_dict)
     print(ranges_list)
-    if theta_in_range(theta, ranges_dict):
+    if theta_in_range(theta, ranges_dict) or theta[3] == 0:
         print('ok SN')
         if fitting_type == 'lum':
             log_likeli = calc_lum_likelihood(theta, data['lum'], ranges_list)
@@ -334,10 +338,13 @@ def plot_lum_with_fit(data, sampler_chain, ranges_dict, step, output_dir, n_walk
     ax.axvspan(-2, 30, alpha=0.1, color='grey')
     log_likeli = []
     x_plotting = np.arange(0, 200, 0.5)
+    print('start of walker loop')
     for i in range(n_walkers):
         [Mzams, Ni, E, R, K, Mix, S, T] = sampler_chain[i, step, :]
         requested = [Mzams, Ni, E, R, K, Mix, S, T]
-        if theta_in_range(requested, ranges_dict):
+        print(requested)
+        print(ranges_list)
+        if theta_in_range(requested, ranges_dict) or R == 0:
             data_x_moved = x_plotting - T
             y_fit = interp_lum.snec_interpolator(requested[0:6], ranges_list, data_x_moved)
             if not isinstance(y_fit, str):
@@ -345,6 +352,7 @@ def plot_lum_with_fit(data, sampler_chain, ranges_dict, step, output_dir, n_walk
                 y_fit = y_fit * S
                 ax.plot(x_plotting, y_fit, alpha=0.4)
                 log_likeli.append(log_likelihood(requested, data, ranges_dict, 'lum'))
+                print(log_likelihood(requested, data, ranges_dict, 'lum'))
     log_likeli = np.average(log_likeli)
     ax.errorbar(data_x, data_y, yerr=[dy0, dy1], marker='o', linestyle='None', color='k')
     results_text = result_text_from_dict(sampler_chain, ranges_dict, SN_name, step, output_dir)
@@ -374,7 +382,7 @@ def plot_mag_with_fit(data, sampler_chain, ranges_dict, step, output_dir, n_walk
     for i in range(n_walkers):
         [Mzams, Ni, E, R, K, Mix, S, T] = sampler_chain[i, step, :]
         requested = [Mzams, Ni, E, R, K, Mix, S, T]
-        if theta_in_range(requested, ranges_dict):
+        if theta_in_range(requested, ranges_dict) or R == 0:
             data_x_moved_all = x_plotting - T
             y_fit = interp_mag.snec_interpolator(requested[0:6], ranges_list, data_x_moved_all, filters)
             if not isinstance(y_fit, str):
@@ -416,10 +424,12 @@ def plot_veloc_with_fit(data, sampler_chain, ranges_dict, step, output_dir, n_wa
     for i in range(n_walkers):
         [Mzams, Ni, E, R, K, Mix, S, T] = sampler_chain[i, step, :]
         requested = [Mzams, Ni, E, R, K, Mix, S, T]
-        if theta_in_range(requested, ranges_dict):
+        if theta_in_range(requested, ranges_dict) or R == 0:
             data_x_moved = x_plotting - T
             y_fit = interp_veloc.snec_interpolator(requested[0:6], ranges_list, data_x_moved)
             if not isinstance(y_fit, str):
+                # multiply whole graph by scaling factor
+                y_fit = y_fit * Sv
                 ax.plot(x_plotting, y_fit, alpha=0.4)
                 log_likeli.append(log_likelihood(requested, data, ranges_dict, 'veloc'))
     log_likeli = np.average(log_likeli)

mcmc_snec_lum.py
@@ -9,7 +9,7 @@ import mcmc_snec
 parts of this code are based on code by Griffin Hosseinzadeh
 '''
 
-SN_name = 'SN2012ec'
+SN_name = 'SN2017eaw'
 
 distances = pd.read_csv(os.path.join('results', 'distances.csv'))
 distance = float(distances.loc[distances['SN_name'] == SN_name]['distance'])
@@ -20,21 +20,28 @@ sigma_S = 2 * distance_err / (distance)
 Mzams_range = [9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0]
 Ni_range = [0.02, 0.07, 0.12, 0.17]
 E_final_range = [0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 1.7]
-Mix_range = [5.0, 8.0]
-R_range = [0.1, 0.1]
-K_range = [0.1, 0.1]
+Mix_range = [2.0, 5.0, 8.0]
+R_range = [0, 0]
+K_range = [0, 0]
+
+# Mzams_range = [9.0, 11.0, 13.0, 15.0, 17.0]
+# Ni_range = [0.02, 0.12]
+# E_final_range = [0.5, 0.9, 1.3, 1.7]
+# Mix_range = [2.0, 8.0]
+# R_range = [1000, 2000]
+# K_range = [0, 50, 100]
 S_range = [1.0-sigma_S, 1.0+sigma_S]
 T_range = [-10, 2]
 
-gaussian_priors = {'S': {'mu': 1.0, 'sigma': sigma_S}}
+nonuniform_priors = {'S': {'gaussian': {'mu': 1.0, 'sigma': sigma_S}}}
 
-n_walkers = 20
-n_steps = 10
+n_walkers = 70
+n_steps = 800
 n_params = 8
-burn_in = 0
+burn_in = 150
 
 time_now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-res_dir = os.path.join('mcmc_results', str(time_now)+'_lum_250days')
+res_dir = os.path.join('mcmc_results', str(time_now)+'_lum_'+SN_name)
 Path(res_dir).mkdir(parents=True, exist_ok=True)
 
 run_param_df = pd.DataFrame.from_dict({'SN_name': SN_name,
@@ -59,7 +66,7 @@ data_lum_w_early= pd.read_csv(os.path.join('results', SN_name+'_martinez.csv'))
 # data_lum_w_early['dLum0'] = data_lum_w_early['dLum0'] * 10**7
 # data_lum_w_early['dLum1'] = data_lum_w_early['dLum1'] * 10**7
 # take only days between 30 and 200 (like the Martinez paper)
-data_lum_w_early = data_lum_w_early.loc[data_lum_w_early['t_from_discovery'] < 250]
+data_lum_w_early = data_lum_w_early.loc[data_lum_w_early['t_from_discovery'] < 200]
 
 # replicate the last point x times to artificially increase weight of fitting to last point (10-folds)
 times_to_amplify = 1
@@ -68,8 +75,8 @@ if times_to_amplify > 1:
     last_row_repeats = pd.concat([last_row]*(times_to_amplify-1), ignore_index=True)
     data_lum_w_early= pd.concat([data_lum_w_early, last_row_repeats], ignore_index=True)
 
-data_lum = data_lum_w_early.loc[data_lum_w_early['t_from_discovery'] > 30]
-
+# data_lum = data_lum_w_early.loc[data_lum_w_early['t_from_discovery'] > 30]
+data_lum = data_lum_w_early
 
 '''
 # running code #
@@ -78,7 +85,8 @@ data_lum = data_lum_w_early.loc[data_lum_w_early['t_from_discovery'] > 30]
 SN_data_all = {'lum': data_lum}
 SN_data_all_w_early = {'lum': data_lum_w_early}
 
-sampler = mcmc_snec.emcee_fit_params(SN_data_all, n_walkers, n_steps, parameter_ranges, 'lum', gaussian_priors)
+
+sampler = mcmc_snec.emcee_fit_params(SN_data_all, n_walkers, n_steps, parameter_ranges, 'lum', nonuniform_priors)
 
 flat_sampler = sampler.get_chain(flat=True)
 np.savetxt(os.path.join(res_dir, 'flat_sampler.csv'), flat_sampler, delimiter=",")
@@ -91,10 +99,9 @@ mcmc_snec.plot_lightcurve_with_fit(sampler.chain, SN_data_all_w_early, parameter
 mcmc_snec.plot_lightcurve_with_fit(sampler.chain, SN_data_all_w_early, parameter_ranges,
                                         'lum', res_dir, n_walkers, SN_name, 0)
 
+mcmc_snec.chain_plots(sampler.chain, parameter_ranges, res_dir, burn_in)
 
-mcmc_snec.chain_plots(sampler, parameter_ranges, res_dir)
-
-mcmc_snec.corner_plot(sampler, burn_in, parameter_ranges, res_dir)
+mcmc_snec.corner_plot(sampler.get_chain(flat=True)[n_walkers*burn_in:-1, :], parameter_ranges, res_dir)
 
 print(sampler.chain.shape)
 

mcmc_snec_lum_twostep.py
@@ -55,10 +55,10 @@ T_range = [-10, 2]
 
 nonuniform_priors = {'S': {'gaussian': {'mu': 1.0, 'sigma': sigma_S}}}
 
-n_walkers = 16
-n_steps = 5
+n_walkers = 70
+n_steps = 400
 n_params = 8
-burn_in = 2
+burn_in = 150
 
 
 run_param_df = pd.DataFrame.from_dict({'SN_name': SN_name,
@@ -124,8 +124,8 @@ parameter_ranges['Mzams'] = [9.0, 11.0, 13.0, 15.0, 17.0]
 parameter_ranges['Ni'] = [0.02, 0.12]
 parameter_ranges['E'] = [0.5, 0.9, 1.3, 1.7]
 parameter_ranges['Mix'] = [2.0, 8.0]
-parameter_ranges['R'] = [0, 2000]
-parameter_ranges['K'] = [0, 100]
+parameter_ranges['R'] = [1000, 2000]
+parameter_ranges['K'] = [0, 20, 50, 100]
 
 last_walkers_step_one = sampler_first_step.get_chain(flat=True)[-(n_walkers+1):-1, :]
 
@@ -140,7 +140,7 @@ last_walkers_step_one[:, 4] = np.random.rand(n_walkers) * \
 sampler_second_step = mcmc_snec.emcee_fit_params(SN_data_all_w_early, n_walkers, n_steps, parameter_ranges, 'lum',
                                                  nonuniform_priors, init_guesses=last_walkers_step_one)
 sampler_chain = np.concatenate((sampler_first_step.chain, sampler_second_step.chain), axis=1)
-param_dict = mcmc_snec.get_param_results_dict(sampler_chain, parameter_ranges, n_steps-1, res_dir)
+param_dict = mcmc_snec.get_param_results_dict(sampler_chain, parameter_ranges, n_steps*2-1, res_dir)
 
 sampler_chain_flat = np.concatenate((
     sampler_first_step.get_chain(flat=True),
@@ -150,7 +150,7 @@ sampler_chain_flat = np.concatenate((
 pd.DataFrame(sampler_chain_flat).to_csv(os.path.join(res_dir, 'flat_sampler.csv'))
 
 mcmc_snec.plot_lightcurve_with_fit(sampler_chain, SN_data_all_w_early, parameter_ranges,
-                                        'lum', res_dir, n_walkers, SN_name, n_steps-1)
+                                        'lum', res_dir, n_walkers, SN_name, n_steps*2-1)
 mcmc_snec.plot_lightcurve_with_fit(sampler_chain, SN_data_all_w_early, parameter_ranges,
                                         'lum', res_dir, n_walkers, SN_name, 0)
 