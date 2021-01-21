c7f28dba90697356f3f1fda60d6b8a87b9e3f3da
Shahar Bracha <shaharbra@gmail.com>
2021-01-21 22:57:37 +0200

    add plotting of the Martinez fits (produced by their hydrodynamical model), improve text box and plotting

--- plot_snec_fits_martinez_and_data.py
+++ plot_snec_fits_martinez_and_data.py
@@ -9,7 +9,7 @@ import snec_result_interpolator_veloc as interp_veloc
 
 
 
-result_t = '2021-01-12_18-59-41_lum_veloc'
+result_t = '2021-01-16_13-54-40_lum_veloc_wSv1.4_SN2018aoq'
 result_dir = os.path.join('mcmc_results', result_t)
 
 flat_sampler_path = os.path.join(result_dir, 'flat_sampler.csv')
@@ -17,6 +17,7 @@ run_params_path = os.path.join(result_dir, 'run_parameters.csv')
 output_path = os.path.join(result_dir, 'run_parameters.csv')
 
 run_params = pd.read_csv(run_params_path, index_col=0).T
+
 
 def import_ranges(list):
     ranges = []
@@ -43,10 +44,13 @@ n_steps = int(run_params.iloc[0]['n_steps'])
 flat_sampler = pd.read_csv(flat_sampler_path, names=['Mzams', 'Ni', 'E', 'R', 'K', 'Mix', 'S', 'T'],
                            skiprows=(n_steps-1) * (n_walkers))
 
+
 martinez_values_path = os.path.join('results', SN_name+'_martinez_values.csv')
 martinez_values = list(pd.read_csv(martinez_values_path, names='val').T.iloc[0].astype(float))
-print(martinez_values)
 
+martinez_fits_path = os.path.join('results', 'martinez_bestfit_models', SN_name+'.dat')
+martinez_fits = pd.read_csv(martinez_fits_path, sep=r'\s+')
+martinez_fits['VPH'] = martinez_fits['VPH'] / 10**5   # cm/s to km/s
 
 data_lum_path = os.path.join('results', SN_name+'_martinez.csv')
 data_veloc_path = os.path.join('results', SN_name+'_expansion_velocities.csv')
@@ -60,6 +64,8 @@ data_veloc.sort_values(by=['t_from_discovery'], inplace=True)
 data_veloc = data_veloc.loc[data_veloc['t_from_discovery'] > 20]
 
 SN_data = {'lum': data_lum, 'veloc': data_veloc}
+# SN_data = {'lum': data_lum}
+
 
 # last_rows = flat_sampler.iloc[-n_walkers:]
 
@@ -91,7 +97,15 @@ def result_text_from_dict(sampler_df):
             res_text += param + ': ' + rounded_str(param_dict[param]) + r'$\pm$ [' +\
                             rounded_str(param_dict[param+'_lower']) + ',' +\
                             rounded_str(param_dict[param+'_upper']) + ']\n'
-    res_text += 'no CSM'
+    if (param_dict['K'] == 0) & (param_dict['R'] == 0):
+        res_text += 'no CSM'
+    else:
+        res_text += 'K' + ': ' + rounded_str(param_dict['K']) + r'$\pm$ [' + \
+                    rounded_str(param_dict['K_lower']) + ',' + \
+                    rounded_str(param_dict['K_upper']) + ']\n'
+        res_text += 'R' + ': ' + rounded_str(param_dict['R']) + r'$\pm$ [' + \
+                    rounded_str(param_dict['R_lower']) + ',' + \
+                    rounded_str(param_dict['R_upper']) + ']\n'
     return res_text
 
 fit_plotting_times = np.concatenate((np.arange(1.0, 2.0, 0.1),
@@ -99,8 +113,6 @@ fit_plotting_times = np.concatenate((np.arange(1.0, 2.0, 0.1),
                                     np.arange(10.0, 100.0, 2.0),
                                     np.arange(100.0, 150.0, 0.5),
                                     np.arange(150.0, 200.0, 2.0)))
-
-print(fit_plotting_times)
 
 
 def plot_lum_with_fit(data, sampler_df, ranges_dict, output_dir, n_walkers, SN_name, martinez_values):
@@ -135,7 +147,14 @@ def plot_lum_with_fit(data, sampler_df, ranges_dict, output_dir, n_walkers, SN_n
     data_x_moved = fit_plotting_times - martinez_values[7]
     y_fit = interp_lum.snec_interpolator(martinez_values[0:6], ranges_list, data_x_moved)
     y_fit = y_fit * martinez_values[6]
-    ax.plot(fit_plotting_times, y_fit, color='green', label='Martinez results', linewidth=2.0)
+    ax.plot(fit_plotting_times, y_fit, color='green', label='Martinez values on SNEC', linewidth=2.0)
+
+    # martinez fits (according to their model)
+    data_x_moved = martinez_fits['TIME'] + martinez_values[7]
+    y_fit = np.power(10, martinez_fits['LOGL'])
+    y_fit = y_fit * martinez_values[6]
+    ax.plot(data_x_moved, y_fit, color='orange', label='Martinez fits', linewidth=2.0)
+
     results_text = 'Martinez results (by SNEC)'+'\n\n' + \
                    'Mzams: ' + str(martinez_values[0])+'\n' + \
                    'Ni: ' + str(martinez_values[1])+'\n' + \
@@ -144,7 +163,7 @@ def plot_lum_with_fit(data, sampler_df, ranges_dict, output_dir, n_walkers, SN_n
                    'S: ' + str(martinez_values[6]) + '\n' + \
                    'T: ' + str(martinez_values[7]) + '\n' + \
                    'noCSM'
-    ax.text(1.02, 0.5, results_text, transform=ax.transAxes, fontsize=14,
+    ax.text(1.02, 0.4, results_text, transform=ax.transAxes, fontsize=14,
             verticalalignment='center', bbox=dict(facecolor='white', alpha=0.5), color='green')
     # real observations for the SN
     ax.errorbar(data_x, data_y, yerr=[dy0, dy1], marker='o', linestyle='None', color='k', label=SN_name+' observations')
@@ -152,9 +171,10 @@ def plot_lum_with_fit(data, sampler_df, ranges_dict, output_dir, n_walkers, SN_n
     ax.set_title(SN_name, fontsize=18)
     ax.legend()
     plt.tight_layout()
-    ax.set_yscale('linear')
-    f_fit.savefig(os.path.join(output_dir, str(result_t) + '_' + SN_name + '_lum.png'))
-    ax.set_ylim(float(0.5 * 10 ** 41), float(1.6 * 10 ** 43))
+    # ax.set_yscale('linear')
+    # f_fit.savefig(os.path.join(output_dir, str(result_t) + '_' + SN_name + '_lum.png'))
+    # ax.set_ylim(float(0.5 * 10 ** 41), float(1.6 * 10 ** 43))
+    ax.set_ylim(np.min(data_y) * 0.5, np.max(data_y) * 5)
     ax.set_yscale('log')
     plt.tight_layout()
     f_fit.savefig(os.path.join(output_dir, str(result_t) + '_' + SN_name + '_lum_log.png'))
@@ -175,6 +195,8 @@ def plot_veloc_with_fit(data, sampler_df, ranges_dict, output_dir, n_walkers, SN
         data_x_moved = x_plotting - T
         y_fit = interp_veloc.snec_interpolator(requested[0:6], ranges_list, data_x_moved)
         if not isinstance(y_fit, str):
+            # multiply whole graph by scaling factor
+            # y_fit = y_fit * 1.4
             if add_label:
                 ax.plot(x_plotting, y_fit, alpha=0.2, color='purple', label='SNEC-MCMC fits')
                 add_label = False
@@ -187,7 +209,12 @@ def plot_veloc_with_fit(data, sampler_df, ranges_dict, output_dir, n_walkers, SN
     data_x_moved = x_plotting - martinez_values[7]
     y_fit = interp_veloc.snec_interpolator(martinez_values[0:6], ranges_list, data_x_moved)
     y_fit = y_fit * martinez_values[6]
-    ax.plot(x_plotting, y_fit, color='green', label='Martinez results', linewidth=2.0)
+    ax.plot(x_plotting, y_fit, color='green', label='Martinez values on SNEC', linewidth=2.0)
+
+    # martinez fits (according to their model)
+    data_x_moved = martinez_fits['TIME'] + martinez_values[7]
+    ax.plot(data_x_moved, martinez_fits['VPH'], color='orange', label='Martinez fits', linewidth=2.0)
+
     results_text = 'Martinez results (by SNEC)' + '\n\n' + \
                    'Mzams: ' + str(martinez_values[0]) + '\n' + \
                    'Ni: ' + str(martinez_values[1]) + '\n' + \
@@ -196,7 +223,7 @@ def plot_veloc_with_fit(data, sampler_df, ranges_dict, output_dir, n_walkers, SN
                    'S: ' + str(martinez_values[6]) + '\n' + \
                    'T: ' + str(martinez_values[7]) + '\n' + \
                    'noCSM'
-    ax.text(1.02, 0.5, results_text, transform=ax.transAxes, fontsize=14,
+    ax.text(1.02, 0.4, results_text, transform=ax.transAxes, fontsize=14,
             verticalalignment='center', bbox=dict(facecolor='white', alpha=0.5), color='green')
     # real observations for the SN
     ax.errorbar(data_x, data_y, yerr=dy, marker='o', linestyle='None', color='k',
