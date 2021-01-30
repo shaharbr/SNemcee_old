cb1ab5dc875fcb4d3d0e08f398c4fd2408ded5b7
Shahar Bracha <shaharbra@gmail.com>
2021-01-30 13:58:48 +0200

    update 30.1.21

--- plot_snec_lum.py
+++ plot_snec_lum.py
@@ -19,17 +19,18 @@ import os
 # plt.savefig(os.path.join('figures',
 #                              'lum_M9.0_Ni0.17_E0.7_R3000_K120.png'))
 
-for M in ['15.0']:
-    for E in ['1.0', '1.3']:
-        for Mix in ['1.0']:
-            plt.figure()
+plt.figure()
+for M in ['13.0']:
+    for E in ['0.9']:
+        for Mix in ['2.0', '5.0', '8.0']:
+
             lum = pd.read_csv(os.path.join('data', 'example_lum_snec',
-                                         'M'+str(M)+'_Ni0.02_E'+str(E)+'_Mix'+Mix+'_R600_K0.001','lum_observed.dat'),
+                                         'M'+str(M)+'_Ni0.07_E'+str(E)+'_Mix'+Mix+'_R0.1_K0.1','lum_observed.dat'),
                             names=['time', 'Lum'], sep=r'\s+')
             # energies = energies.iloc[1:]
             # convert sec to days
             lum['time'] = lum['time'] / 86400
-            plt.plot(lum['time'], lum['Lum'])
+            plt.plot(lum['time'], lum['Lum'], label=Mix)
             plt.legend()
             plt.xlim(-2, 200)
             plt.xlabel('time (days, log)')
@@ -37,9 +38,9 @@ for M in ['15.0']:
             plt.yscale('log')
             # plt.xscale('log')
             # plt.ylim(float(5*10**40), float(3.5*10**42))
-            plt.title('M'+str(M)+'_Ni0.02_E'+str(E)+'_Mix'+Mix+'_R600_K0.001')
+            plt.title('M'+str(M)+'_Ni0.07_E'+str(E)+'_Mix'+Mix+'_R0.1_K0.1')
             plt.tight_layout()
             plt.savefig(os.path.join('figures',
-                                         'lum_logY_M'+str(M)+'_Ni0.02_E'+str(E)+'_Mix'+Mix+'_R600_K0.001.png'))
+                                         'lum_logY_M'+str(M)+'_Ni0.07_E'+str(E)+'_Mix'+Mix+'_R0.1_K0.1.png'))
 
 plt.show()
