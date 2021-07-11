46cd5905d911d4fd4520074b5e01ae17880b0918
Shahar <shaharbra@gmail.com>
2021-07-11 11:08:11 -0400

    change SN to plot

--- plot_snec_magnitudes.py
+++ plot_snec_magnitudes.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-filepath = os.path.join('..', 'all_snec_data', 'M13.0_Ni0.1_E1.5_Mix3.0_R600_K0.001', 'magnitudes.dat')
+filepath = os.path.join('..', 'all_snec_data', 'M15.0_Ni0.02_E0.5_Mix2.0_R0_K0', 'magnitudes.dat')
 
 mag = pd.read_csv(filepath, names=['time', 'Teff', 'PTF_R_AB', 'u', 'g', 'r', 'i', 'z', 'U', 'B', 'V', 'R', 'I'], sep=r'\s+')
 # convert seconds to days
@@ -14,6 +14,7 @@ for filt in ['PTF_R_AB', 'u', 'g', 'r', 'i', 'z', 'U', 'B', 'V', 'R', 'I']:
     ax.plot(mag['time'], mag[filt], label=filt)
     ax.invert_yaxis()
     ax.legend()
+    ax.set_title('M15.0_Ni0.02_E0.5_Mix2.0_R0_K0')
 
 
 plt.show()
